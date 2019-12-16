f832641a9815d2a17b8da8b0990bc50ea55fc2fd
richknicks <rlynch@gmail.com>
2019-12-16 16:20:45 -0500

    test

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject() { 
+  
 }
+
+
 
 /**
  * ### Challenge `getName`
